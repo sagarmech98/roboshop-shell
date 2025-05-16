586e68e0783587ace19a7af61c9fc820006c8636
haneeshm <150122523+sagarmech98@users.noreply.github.com>
2025-05-16 22:39:30 +0530

    fix: refactor cart and catalogue scripts to use common functions for nodejs installation and systemd setup

--- cart.sh
+++ cart.sh
@@ -1,6 +1,9 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+component_name=cart
+source common.sh
+
+nodejs
+
+
 
 cp cart.service /etc/systemd/system/cart.service
 
@@ -12,6 +15,4 @@ unzip /tmp/cart.zip
 cd /app 
 npm install 
 
-systemctl daemon-reload
-systemctl enable cart 
-systemctl start cart
+systemd_setup
--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,9 @@
 #Install NodeJS, By default NodeJS 16 is available, We would like to enable 20 version and install list.
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+component_name=catalogue
+source common.sh
+
+nodejs
+
 
 cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
@@ -13,9 +15,9 @@ unzip /tmp/catalogue.zip
 
 cd /app 
 npm install 
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl start catalogue
+
 
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.vidyapractice.shop </app/db/master-data.js
+
+systemd_setup
